44ba8d64e8b71ba523068c68379a1edd5c651a30
Charlie Forkish <charlie@carnivorestudios.com>
2018-01-04 18:01:44 -0800

    added isEndDrawerOpen and closeDrawer()

--- packages/flutter/lib/src/material/scaffold.dart
+++ packages/flutter/lib/src/material/scaffold.dart
@@ -1076,6 +1076,8 @@ class ScaffoldState extends State<Scaffold> with TickerProviderStateMixin {
 
   bool get isDrawerOpen => _drawerKey.currentState?.isOpen ?? false;
 
+  bool get isEndDrawerOpen => _endDrawerKey.currentState?.isOpen ?? false;
+
   /// Opens the [Drawer] (if any).
   ///
   /// If the scaffold has a non-null [Scaffold.drawer], this function will cause
@@ -1106,6 +1108,10 @@ class ScaffoldState extends State<Scaffold> with TickerProviderStateMixin {
   /// See [Scaffold.of] for information about how to obtain the [ScaffoldState].
   void openEndDrawer() {
     _endDrawerKey.currentState?.open();
+  }
+
+  void closeDrawer() {
+    _drawerKey.currentState?.close();
   }
 
   void closeEndDrawer() {
